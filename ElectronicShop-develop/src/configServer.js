91d218ba9f51fcf05a76c09f788e3635064ae4c4
Katruschenko Artem <52026835+Astro2k19@users.noreply.github.com>
2022-05-23 11:28:24 +0300

    Delete ElectronicShop-develop directory

--- ElectronicShop-develop/src/configServer.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-    mongoUri: "mongodb+srv://Admin:admin@cluster0.s3fgw.mongodb.net/ElectronicShop?retryWrites=true&w=majority",
-    port: 5556,
-    secretKey: 'sup3r_s3cret_k3y'
-};
-
-export default { config };
